bc4a8adae2c201773f0a961eac08431be1db43bb
Julia Karlsson <karlssonjulia.m@gmail.com>
2019-12-13 18:01:05 +0100

    Added functionality to log in and logout

--- app/users/login.php
+++ app/users/login.php
@@ -4,4 +4,28 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-// In this file we login users.
+if(isset($_POST['email'], $_POST['password'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+    $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+
+
+    $user= $statement->fetch(PDO::FETCH_ASSOC);
+
+    if (!$user) {
+        redirect('/login.php');
+    }
+
+    if (password_verify($_POST['password'], $user['password'])) {
+        // If the password was valid we know that the user exists and provided
+        // the correct password. We can now save the user in our session.
+        // Remember to not save the password in the session!
+        unset($user['password']);
+
+        $_SESSION['user'] = $user;
+    }
+}
+
+redirect('/');
