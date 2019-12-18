5be618fdc846a9255a5067dc350ea76878de5226
Julia Karlsson <karlssonjulia.m@gmail.com>
2019-12-18 16:33:39 +0100

    Update login.php

--- app/users/login.php
+++ app/users/login.php
@@ -7,12 +7,11 @@ require __DIR__.'/../autoload.php';
 if(isset($_POST['email'], $_POST['password'])) {
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+    $statement = $pdo->prepare('SELECT * FROM user WHERE email = :email');
     $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
-
-
-    $user= $statement->fetch(PDO::FETCH_ASSOC);
+    
+    $user=$statement->fetch(PDO::FETCH_ASSOC);
 
     if (!$user) {
         redirect('/login.php');
@@ -22,9 +21,11 @@ if(isset($_POST['email'], $_POST['password'])) {
         // If the password was valid we know that the user exists and provided
         // the correct password. We can now save the user in our session.
         // Remember to not save the password in the session!
+        
         unset($user['password']);
-
+        
         $_SESSION['user'] = $user;
+        
     }
 }
 
