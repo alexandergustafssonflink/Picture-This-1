efa86a0dbb302977acb6a1a7046a3f0dfe7c5887
Julia Karlsson <karlssonjulia.m@gmail.com>
2019-12-18 16:33:45 +0100

    Update logout.php

--- app/users/logout.php
+++ app/users/logout.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+echo 'woho';
+
 // Remove the user session variable and redirect the user back to the homepage.
-unset($_SESSION['user']);
+session_destroy();
+
 redirect('/');
