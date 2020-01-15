bd856d8b90c203a9cccc07a1fd0e82718bff69aa
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-01-15 15:16:01 +0100

    Update myaccount.php

--- myaccount.php
+++ myaccount.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['user'])){
     <?php endif;?>
         <form action="app/users/upload-profile-picture.php" method="post" enctype="multipart/form-data">
         <div class="fileinputs">
-        <?php if ($avatar['data']!== NULL):?>
+        <?php if ($avatar):?>
             <img src="<?php echo "/app/users/uploads/".$avatar['data'];?>" alt="avatar image" class="avatar" loading="lazy">
             <?php else : ?> 
                 <p>Upload your profile avatar image</p>
@@ -25,9 +25,8 @@ if (!isset($_SESSION['user'])){
             
             <input type="file" name="profile-img" accept="image/*" class="choose-file" loading="lazy">
             <input type="submit" class="upload-button" value="Upload"></input> 
-            
         </div> <!-- /fileinputs -->
-    <p class="edit-text"> Edit your account email, password and biography.</p>
+        <p class="edit-text"> Edit your account email, password and biography.</p>
             </form>
             
     
@@ -47,7 +46,7 @@ if (!isset($_SESSION['user'])){
         <div class="form-group">
             <label for="biography">Biography</label>
             <textarea type="text" name="biography" id="biography" cols="30" rows="5" placeholder="Bio"><?php echo $user['biography']?></textarea>
-            <input type="submit" class="submit-button" value="Submit"></input>
+            <input type="submit" class="submit-button" value="Update"></input>
         </div><!-- /form-group -->
     </form>
     </article> <!-- /edit-wrapper -->
